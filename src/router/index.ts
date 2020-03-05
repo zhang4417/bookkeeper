3de6c0c0fbe6d201b6e48768092cc9ffcf42e801
zhang4417 <1804815164@qq.com>
2020-03-05 15:51:55 +0800

    用router布局nav

---
+++ src/router/index.ts
@@ -0,0 +1,32 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Labels from "@/views/Labels.vue"
+import Money from "@/views/Money.vue"
+import Statistics from "@/views/Statistics.vue"
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: "/",
+    redirect: '/money'
+  },
+  {
+    path: '/labels',
+    component: Labels
+  },
+  {
+    path: '/money',
+    component: Money
+  },
+  {
+    path: '/statistics',
+    component: Statistics
+  }
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
---
+++ src/views/Statistics.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>statistics</div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({});
+</script>
+
+<style lang="scss" scoped>
+</style>
