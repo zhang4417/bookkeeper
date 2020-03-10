461073cbfb4e77284751a4ad04fdc8c7178fb006
zhang4417 <1804815164@qq.com>
2020-03-10 17:29:05 +0800

    从'vue-property-decorator'引用@Prop

--- src/components/Money/Types.vue
+++ src/components/Money/Types.vue
@@ -7,17 +7,21 @@
 
 <script lang="ts">
 import Vue from "vue";
-import { Component } from "vue-property-decorator";
+import { Component, Prop } from "vue-property-decorator";
 
 @Component
 export default class Types extends Vue {
   type = "-";
+  @Prop(String) xxx: string | undefined;
   selectType(type: string) {
     if (type !== "-" && type !== "+") {
       throw new Error("type is unknown");
     } else {
       this.type = type;
     }
+  }
+  mounted() {
+    console.log(this.xxx);
   }
 }
 </script>
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -2,7 +2,7 @@
   <div>
     <Layout prefix="layout">
       <NumberPad />
-      <Types xxx='hi'/>
+      <Types xxx="hi" />
       <Notes />
       <Tags />
     </Layout>
